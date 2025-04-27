bd18b57544d2f3e80e0e6a1abd79e414fc8ed658
aqilulkamal <aqilulkamal@gmail.com>
2025-04-27 19:51:18 +0700

    PUSING PAK

---
+++ app/surat.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function surat() {
+  return (
+    <View style={styles.container}>
+      <Text>surat</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: '20%',
+    }
+})
